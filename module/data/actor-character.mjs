a03f1c0564c234be04e260500561f6d3efc78113
xiajiefvtt <xiajiefvtt@outlook.com>
2025-03-14 18:40:28 +0800

    basic info done

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -7,6 +7,9 @@ export default class XiaJieFvttCharacter extends XiaJieFvttActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
+    schema.name2_1 = new fields.StringField({ required: false, blank: true });
+    schema.name2_2 = new fields.StringField({ required: false, blank: true });
+
     schema.attributes = new fields.SchemaField({
       level: new fields.SchemaField({
         value: new fields.NumberField({ ...requiredInteger, initial: 1 })
